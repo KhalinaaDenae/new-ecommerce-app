f62718808a7fb31b272a3c9a7cf08063074df0d6
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-08-06 21:49:29 -0400

    changed uploads path

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,9 @@ app.get('/api/config/paypal', (req, res) =>
 
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
+    // Serve static images from the 'uploads' folder in the root directory
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
